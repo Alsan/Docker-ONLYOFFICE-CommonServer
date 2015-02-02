c4d087dfe30443c9bcf2e2c9fcefdf900ce874ca
root <alexey.bannov@onlyoffice.com>
2015-02-02 10:22:14 +0000

    Support for HTTPS. Using the latest version of Mono.

--- Dockerfile
+++ Dockerfile
@@ -6,20 +6,25 @@ ENV LANGUAGE en_US:en
 ENV LC_ALL en_US.UTF-8 
 
 RUN echo "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d && \
-    echo "deb http://download.onlyoffice.com/repo/debian squeeze main" >>  /etc/apt/sources.list && \
+    echo "deb http://static.teamlab.com.s3.amazonaws.com/repo/debian/ squeeze main" >>  /etc/apt/sources.list && \
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D9D0BF019CC8AC0D && \
+    echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list && \
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF && \
     DEBIAN_FRONTEND=noninteractive  && \
     locale-gen en_US.UTF-8 && \
     apt-get update && \
-    apt-get install -yq onlyoffice && \
+    apt-get install --force-yes -yq onlyoffice && \
     rm -rf /var/lib/apt/lists/*
 
-ADD run-onlyoffice.sh /run-onlyoffice.sh
-RUN chmod 755 /*.sh
+ADD config /app/onlyoffice/setup/config/
+ADD run-onlyoffice.sh /app/onlyoffice/run-onlyoffice.sh
+RUN chmod 755 /app/onlyoffice/*.sh
 
 VOLUME ["/var/log/onlyoffice"]
+VOLUME ["/var/www/onlyoffice/Data"]
+VOLUME ["/var/lib/mysql"]
 
 EXPOSE 80
 EXPOSE 443
 
-CMD bash -C '/run-onlyoffice.sh';'bash'
+CMD bash -C '/app/onlyoffice/run-onlyoffice.sh';'bash'
--- run-onlyoffice.sh
+++ run-onlyoffice.sh
@@ -1,5 +1,57 @@
-#!/bin/bash
+#/bin/bash
 
+DATA_DIR="/var/www/onlyoffice/Data"
+LOG_DIR="/var/log/onlyoffice/8.1"
+
+ONLYOFFICE_HTTPS=${ONLYOFFICE_HTTPS:-false}
+
+SSL_CERTIFICATES_DIR="${DATA_DIR}/certs"
+SSL_CERTIFICATE_PATH=${SSL_CERTIFICATE_PATH:-${SSL_CERTIFICATES_DIR}/onlyoffice.crt}
+SSL_KEY_PATH=${SSL_KEY_PATH:-${SSL_CERTIFICATES_DIR}/onlyoffice.key}
+SSL_DHPARAM_PATH=${SSL_DHPARAM_PATH:-${SSL_CERTIFICATES_DIR}/dhparam.pem}
+SSL_VERIFY_CLIENT=${SSL_VERIFY_CLIENT:-off}
+ONLYOFFICE_HTTPS_HSTS_ENABLED=${ONLYOFFICE_HTTPS_HSTS_ENABLED:-true}
+ONLYOFFICE_HTTPS_HSTS_MAXAGE=${ONLYOFFICE_HTTPS_HSTS_MAXAG:-31536000}
+SYSCONF_TEMPLATES_DIR="/app/onlyoffice/setup/config"
+
+# stop services
+service mysql stop
+service monoserve stop
+service nginx stop
+
+# setup HTTPS
+if [ -f "${SSL_CERTIFICATE_PATH}" -a -f "${SSL_KEY_PATH}" ]; then
+	cp ${SYSCONF_TEMPLATES_DIR}/nginx/onlyoffice-ssl /etc/nginx/sites-enabled/onlyoffice
+           
+        mkdir ${LOG_DIR}/nginx
+
+	# configure nginx
+	sed 's,{{SSL_CERTIFICATE_PATH}},'"${SSL_CERTIFICATE_PATH}"',' -i /etc/nginx/sites-enabled/onlyoffice
+	sed 's,{{SSL_KEY_PATH}},'"${SSL_KEY_PATH}"',' -i /etc/nginx/sites-enabled/onlyoffice
+
+	# if dhparam path is valid, add to the config, otherwise remove the option
+	if [ -r "${SSL_DHPARAM_PATH}" ]; then
+	  sed 's,{{SSL_DHPARAM_PATH}},'"${SSL_DHPARAM_PATH}"',' -i /etc/nginx/sites-enabled/onlyoffice
+	else
+	  sed '/ssl_dhparam {{SSL_DHPARAM_PATH}};/d' -i /etc/nginx/sites-enabled/onlyoffice
+	fi
+
+	sed 's,{{SSL_VERIFY_CLIENT}},'"${SSL_VERIFY_CLIENT}"',' -i /etc/nginx/sites-enabled/onlyoffice
+
+	if [ -f /usr/local/share/ca-certificates/ca.crt ]; then
+	  sed 's,{{CA_CERTIFICATES_PATH}},'"${CA_CERTIFICATES_PATH}"',' -i /etc/nginx/sites-enabled/onlyoffice
+	else
+	  sed '/{{CA_CERTIFICATES_PATH}}/d' -i /etc/nginx/sites-enabled/onlyoffice
+	fi
+
+	if [ "${ONLYOFFICE_HTTPS_HSTS_ENABLED}" == "true" ]; then
+	  sed 's/{{ONLYOFFICE_HTTPS_HSTS_MAXAGE}}/'"${ONLYOFFICE_HTTPS_HSTS_MAXAGE}"'/' -i /etc/nginx/sites-enabled/onlyoffice
+	else
+	  sed '/{{ONLYOFFICE_HTTPS_HSTS_MAXAGE}}/d' -i /etc/nginx/sites-enabled/onlyoffice
+	fi
+fi
+
+# start services
 service mysql start
 service monoserve start
 service nginx start
@@ -9,4 +61,5 @@ service onlyofficeJabber start
 service onlyofficeMailAggregator start
 service onlyofficeMailWatchdog start
 service onlyofficeNotify start
+#service onlyofficeBackup start
 
